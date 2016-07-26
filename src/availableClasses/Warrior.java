3724fd2ebee051cf8179ef5edc629531b56fbcd0
volen1 <planbshredder@gmail.com>
2016-07-26 14:45:50 -0400

    Added Warrior health scaling v0.0.2

--- src/availableClasses/Warrior.java
+++ src/availableClasses/Warrior.java
@@ -1,13 +1,30 @@
 package availableClasses;
 
+import java.math.*;
 
 public class Warrior {
-    public Warrior(int level, String weapon, String armor)
+    public Warrior(int level)
     {
-        int health = 10;
+        double health = 10;
         if(level <=5)
         {
-
+            health = health * level;
+        }
+        else if(level > 5 && level <= 10)
+        {
+            health = health*level*1.25;
+        }
+        else if(level > 10 && level <= 15)
+        {
+            health = health*level*1.5;
+        }
+        else if(level > 15 && level <= 20)
+        {
+            health = Math.pow(health, 2) + (level*2);
+        }
+        else
+        {
+            health = Math.pow(health, 2) + (level*2) * 1.25;
         }
         return;
     }
--- src/textAdventure.java
+++ src/textAdventure.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 
 public class textAdventure {
-    public static void main(String argsp[]){
+    public static void main(String args[]){
         Scanner s = new Scanner(System.in);
 
         System.out.println("Welcome to voleN\'s Text RPG!");
